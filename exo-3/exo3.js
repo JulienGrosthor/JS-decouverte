42557a4bc7ea945d1b0679d3526518c9d2a15aa3
Julien Grosthor <julien.grosthor@le-campus-numerique.fr>
2024-09-02 16:40:04 +0200

    exercice 3 en cours

--- exo-3/exo3.js
+++ exo-3/exo3.js
@@ -1,2 +1,33 @@
 // you can write js here
 console.log('exo-3');
+
+let playerInput = prompt("Your choice ( rock, paper, scissors ) ?").toLowerCase();
+let playerChoice = getPlayerChoice(playerInput);
+let computerChoice = getComputerChoice();
+
+function getPlayerChoice(playerInput) {
+    if (playerInput === 'rock' || 'paper' || 'scissors') {
+        return playerInput;
+    } else {
+        console.log('MESSAGE: INPUT ERROR');
+    }
+}
+
+function getComputerChoice() {
+    let randomNumber = Math.floor(Math.random() * (2 - 0) + 0);
+    switch (randomNumber) {
+        case 0:
+            return 'rock';
+            break;
+        case 1:
+            return 'paper';
+            break;
+        case 2:
+            return 'scissors';
+            break;
+    }
+}
+
+function findWinner(playerChoice, computerChoice) {
+
+}
