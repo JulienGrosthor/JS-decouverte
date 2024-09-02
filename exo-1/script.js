defca8bab7642f3afc8fec4d35885782ed6c27bc
Julien Grosthor <julien.grosthor@le-campus-numerique.fr>
2024-09-02 12:25:57 +0200

    exercice 1 fini

--- exo-1/script.js
+++ exo-1/script.js
@@ -1,2 +1,20 @@
 // you can write js here
 console.log('hello from file');
+
+//fait qu'un pop-up apparait avec la question, et on peut rentrer une valeur qu'on veut, le reste du code s'applique par rapport au résultat ensuite!)
+var kelvins = prompt("Quelle est la température en Kelvins aujourd'hui ?");let celsius;
+
+function kelvinsToCelcius(kelvins){ //pour convertir, on vient déduire 273 à kelvin.
+    return kelvins - 273;
+}
+
+celsius = kelvinsToCelcius(kelvins);
+console.log('temperature in celsius = ' + celsius);
+
+let fahrenheit = celsiustoFahrenheit(celsius);
+
+function celsiustoFahrenheit(celsius){
+    return celsius * (9/5) + 32
+}
+console.log('temperature in fahrenheit = ' + Math.floor(fahrenheit));
+
