66135fe6dc7f7cf40225282a781135b86239967a
Julien Grosthor <julien.grosthor@le-campus-numerique.fr>
2024-09-04 16:40:31 +0200

    Ãdernière modif exercice 7

--- exo-7/exo7.js
+++ exo-7/exo7.js
@@ -1,5 +1,35 @@
 console.log("exo-7");
 
+
+
+//Parcourir les articles fournis pour les afficher un à un dans la console
+for (let product of jsonDatas) {
+    console.log(product);
+}
+
+//Créer un objet qui servira à traduire les types. Cet objet aura pour structure {type : "type traduit"}. Par exemple {videoGame:"Jeux Vidéos"}.
+let types = {
+    car: "voiture",
+    house: "maison",
+    game: "jeu",
+    videoGame: "jeu vidéo",
+    show: "Spectacle"
+}
+
+//Ajouter une nouvelle clé aux objets existants pour stocker le type traduit, sans modifier le fichier data.js. Répéter l'étape 1 pour vérifier le type traduit.
+let addTraductType = () => {
+    for (let product of jsonDatas) {
+        for (let type in types) {
+            if (product.type === type) {
+                product.typeTraduction = types[type];
+            }
+        }
+    }
+}
+addTraductType();
+displayProducts(jsonDatas)
+
+
 function displayProducts(productList) {
 //Créer une page HTML et lister les articles depuis la donnée.
     let catalog = document.getElementById("catalog");
@@ -17,39 +47,6 @@ function displayProducts(productList) {
 
 
 console.log(jsonDatas);
-
-//Parcourir les articles fournis pour les afficher un à un dans la console
-for (let product of jsonDatas) {
-    console.log(product);
-}
-
-
-
-//Ajouter une nouvelle clé aux objets existants pour stocker le type traduit, sans modifier le fichier data.js. Répéter l'étape 1 pour vérifier le type traduit.
-let addTraductType = () => {
-
-    //Créer un objet qui servira à traduire les types. Cet objet aura pour structure {type : "type traduit"}. Par exemple {videoGame:"Jeux Vidéos"}.
-    let types = {
-        car: "voiture",
-        house: "maison",
-        game: "jeu",
-        videoGame: "jeu vidéo",
-        show: "Spectacle"
-    }
-
-    for (let product of jsonDatas) {
-        for (let type in types) {
-            if (product.type === type) {
-                product.typeTraduction = types[type];
-            }
-        }
-    }
-}
-addTraductType();
-displayProducts(jsonDatas)
-
-
-
 
 
 
