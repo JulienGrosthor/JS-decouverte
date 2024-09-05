3a3c6b37b133054ee6f033b85fa23db5f39bbdbd
Julien Grosthor <julien.grosthor@le-campus-numerique.fr>
2024-09-05 12:03:50 +0200

    exercice 7 en cours, avant nouveau code

--- exo-7/exo7.js
+++ exo-7/exo7.js
@@ -2,12 +2,12 @@ console.log("exo-7");
 
 
 
-//Parcourir les articles fournis pour les afficher un à un dans la console
+//1. Parcourir les articles fournis pour les afficher un à un dans la console
 for (let product of jsonDatas) {
     console.log(product);
 }
 
-//Créer un objet qui servira à traduire les types. Cet objet aura pour structure {type : "type traduit"}. Par exemple {videoGame:"Jeux Vidéos"}.
+//2. Créer un objet qui servira à traduire les types. Cet objet aura pour structure {type : "type traduit"}. Par exemple {videoGame:"Jeux Vidéos"}.
 let types = {
     car: "voiture",
     house: "maison",
@@ -16,7 +16,7 @@ let types = {
     show: "Spectacle"
 }
 
-//Ajouter une nouvelle clé aux objets existants pour stocker le type traduit, sans modifier le fichier data.js. Répéter l'étape 1 pour vérifier le type traduit.
+//3. Ajouter une nouvelle clé aux objets existants pour stocker le type traduit, sans modifier le fichier data.js. Répéter l'étape 1 pour vérifier le type traduit.
 let addTraductType = () => {
     for (let product of jsonDatas) {
         for (let type in types) {
@@ -27,28 +27,31 @@ let addTraductType = () => {
     }
 }
 addTraductType();
-displayProducts(jsonDatas)
 
-
+//4. Créer une page HTML et lister les articles depuis la donnée.
 function displayProducts(productList) {
-//Créer une page HTML et lister les articles depuis la donnée.
     let catalog = document.getElementById("catalog");
     for (let product of productList) {
         catalog.innerHTML += `
         <h3>${product.name}</h3>
         <ul>
-            <li>${product.type}</li>
-            <li>${product.description}</li>
-            <li>${product.price}</li>
-            <li>${product.quantity}</li>
+            <li>Type: ${product.typeTraduction}</li>
+            <li>Description: ${product.description}</li>
+            <li>Prix: ${product.price}</li>
+            <li>Quantité: ${product.quantity}</li>
         </ul>`
     }
 }
 
-
-console.log(jsonDatas);
-
+displayProducts(jsonDatas)
 
 
-
-
+//5. Créer un champ texte et un bouton. Au clic sur le bouton, afficher les articles du type entré dans le champ texte.
+    let divForm = document.getElementById("sort");
+        sort.innerHTML = `
+        <form action="" method="post">
+            <label for="productType">Product type filter:</label>
+            <input type="text" id="productType" name="productType" required>
+            <input type="submit" value="Submit"</input>
+        </form>
+        `
